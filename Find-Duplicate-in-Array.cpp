0feebd7da9ee29b84e4d7b6b7cd6200dac5b30b6
Neer393 <108523406+Neer393@users.noreply.github.com>
2023-06-08 14:45:53 +0530

    Create Find-Duplicate-in-Array.cpp

---
+++ Find-Duplicate-in-Array.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	unordered_map<int,int>mp;
+	for(auto i:arr)	mp[i]++;
+	for(auto i:mp){
+		if(i.second>1)	return i.first;
+	}
+	return -1;
+}
