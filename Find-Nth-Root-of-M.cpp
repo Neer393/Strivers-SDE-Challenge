a038270924c9c9b9e9e97532be7d8ccf102702b3
Neer393 <108523406+Neer393@users.noreply.github.com>
2023-06-10 12:56:31 +0530

    Create Find-Nth-Root-of-M.cpp

---
+++ Find-Nth-Root-of-M.cpp
@@ -0,0 +1,10 @@
+int NthRoot(int n, int m) {
+  int s=1,e=m;
+  while(s<=e){
+    int mid = s+((e-s)/2);
+    if(pow(mid,n)==m) return mid;
+    else if(pow(mid,n)>m)  e=mid-1;
+    else  s=mid+1;
+  }
+  return -1;
+}
