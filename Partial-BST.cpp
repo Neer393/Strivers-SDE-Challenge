197ccb36ff05513fd573e4c1cecd3fb83b4ffdc2
Neer393 <108523406+Neer393@users.noreply.github.com>
2023-06-17 14:37:36 +0530

    Create Partial-BST.cpp

---
+++ Partial-BST.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+bool solve(BinaryTreeNode<int> *root,int mini,int maxi){
+    if(!root)   return true;
+    return root->data>=mini && root->data<=maxi && solve(root->left,mini,root->data) && solve(root->right,root->data,maxi);
+}
+
+bool validateBST(BinaryTreeNode<int> *root) {
+    return solve(root,INT_MIN,INT_MAX);
+}
